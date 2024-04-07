02a6e421263f58043a921fd80daa8543bd2837e9
PoWeR1337 <nfs.zocker36@gmail.com>
2024-04-07 20:32:38 +0200

    update

--- AW_Lib/Class1.cs
+++ AW_Lib/Class1.cs
@@ -1,7 +1,18 @@
 ﻿namespace AW_Lib
 
+    /// AW Libary 
+   ///Update : 7.4.24
+   ///ToDo :
+   ///1 APP Info -> Logging, Version
+   ///2.Telegram bot implentierung
+   ///3.  
+
 {
-        public interface IAppInfo
+
+     /// <summary>
+    /// APP Info
+    /// </summary>
+    public interface IAppInfo
         {
             string Title { get; set; }
             string Version { get; set; }
@@ -12,6 +23,18 @@
         public string Title { get; set; } = "AW-E";
         public string Version { get; set; } = "0.0";
         }
+    // ENDE INFO
+
+
+    /// <summary>
+    /// Telegram
+    /// 
+    /// </summary>
+    class A_Bot
+    {
+        
+    }
+
 }
 
     
