e74712c4cbe5b38aee8ec231870fd3fd52410e5f
PoWeR1337 <nfs.zocker36@gmail.com>
2024-04-07 20:16:15 +0200

    Projektdateien hinzufügen.

---
+++ AW_Lib/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace AW_Lib
+
+{
+        public interface IAppInfo
+        {
+            string Title { get; set; }
+            string Version { get; set; }
+        }
+
+        public class AppInfo : IAppInfo
+        {
+        public string Title { get; set; } = "AW-E";
+        public string Version { get; set; } = "0.0";
+        }
+}
+
+    
+
+
---
+++ Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using Spectre.Console;
+using AW_Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        H_Menu();
+    }
+
+    
+
+   public static void H_Menu()
+    {
+        // Erstelle eine Instanz der Klasse, die das Interface implementiert
+        IAppInfo appInfo = new AppInfo();
+
+        string Titel =  appInfo.Title;
+        appInfo.Title = "AWET";
+        appInfo.Version = "0.1";
+
+
+        // Header with AWET
+        var header = new FigletText(appInfo.Title)
+            .Centered()
+            .Color(Color.Blue);
+
+        // Version separator
+        var versionSeparator = new Rule($"[red]{appInfo.Version}[/]")
+            .Centered();
+
+        // Create a menu
+        var menu = new SelectionPrompt<string>()
+            .Title("[blue]Menü:[/]")
+            .PageSize(5)
+            .AddChoices(new[] { "Option 1", "Option 2", "Option 3" });
+
+        // Render the interface
+        AnsiConsole.Write(header);
+        AnsiConsole.Write(versionSeparator);
+        var selectedOption = AnsiConsole.Prompt(menu);
+
+        // Output the selected option
+        AnsiConsole.MarkupLine($"[yellow]STarte:[/] {selectedOption}");
+
+    }
+}
