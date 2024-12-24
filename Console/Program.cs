b1dea2d6e97e7e6ecefd37c052c5fd68e065d526
Andre <nfs.zocker36@gmail.com>
2024-12-24 02:30:34 +0000

    Adds launch and settings configuration files
    Includes launch.json file for .NET Core debugging configurations
    Adds settings.json file to disable DockerRun dockerrc
    
    Relates to #123

--- Console/Program.cs
+++ Console/Program.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using static Database;
+using Telegram.Bot.Requests;
 
 class Program
 {
@@ -52,79 +53,80 @@ public class DB
 {
     private static DatabaseService _dbService;
 
-    static DB()
-    {
-        _dbService = new DatabaseService("mongodb+srv://power:Joanna1337,,.@aw.71zfrso.mongodb.net/?retryWrites=true&w=majority&appName=aw", "aw");
-    }
-
     public static void Connect()
     {
-        if (_dbService == null)
+        try
         {
-            AnsiConsole.MarkupLine("[red]Database service is not initialized![/]");
-            return;
-        }
+          //  _dbService = new DatabaseService("mongodb+srv://power:Joanna1337,,.@aw.71zfrso.mongodb.net/?retryWrites=true&w=majority&appName=aw", "aw");
 
-        if (!_dbService.Ping())
-        {
-            AnsiConsole.MarkupLine("[red]Database connection failed![/]");
-        }
-        else
-        {
-            AppInfo appInfo = new AppInfo();
-            appInfo.DBConnect = true;
-            appInfo.On = "Online";
-            // Load tools from the database
-            var toolsCollection = _dbService.GetCollection<Tool>("Tool");
-            var tools = toolsCollection.Find(tool => true).ToList();
-
-            // Display tool menu
-            var toolMenu = new SelectionPrompt<string>()
-               .Title("Wähle [green]Tool[/] für Details : ")
-               .AddChoices(tools.Select(t => t.Name).ToArray());
-
-            var selectedToolName = AnsiConsole.Prompt(toolMenu);
-            var selectedTool = tools.First(t => t.Name == selectedToolName);
-
-            // Display subtool menu
-            var subtoolMenu = new SelectionPrompt<string>()
-                .Title($"Wähle [green]Subtool[/] aus [blue]{selectedTool.Name}[/] für Details / Start : ")
-                .AddChoices(selectedTool.Subtools.Select(st => st.Name).ToArray());
-
-            var selectedSubtoolName = AnsiConsole.Prompt(subtoolMenu);
-            var selectedSubtool = selectedTool.Subtools.First(st => st.Name == selectedSubtoolName);
-
-            // Display subtool details or start subtool
-            var subtoolActionMenu = new SelectionPrompt<string>()
-                .Title($"Program : [blue]{selectedSubtool.Name}[/]?")
-                .AddChoices(new[] { "Details", "Start" });
-
-            var subtoolAction = AnsiConsole.Prompt(subtoolActionMenu);
-
-            switch (subtoolAction)
+            if (!_dbService.Ping())
             {
-                case "Details":
-                    // Display subtool details
-                    var table = new Table();
-                    table.AddColumn("Property");
-                    table.AddColumn("Value");
-
-                    table.AddRow("Name", selectedSubtool.Name);
-                    table.AddRow("Update", selectedSubtool.Update);
-                    table.AddRow("Author", selectedSubtool.Author);
-                    table.AddRow("GitHub", selectedSubtool.GitHub);
-
-                    AnsiConsole.Write(table);
-                    Console.ReadLine();
-                    Console.Clear();
-                    break;
-                case "Start":
-                    // Start the subtool
-                    Console.WriteLine($"Starting [blue]{selectedSubtool.Name}[/]...");
-                    // Execute the subtool
-                    ExecuteSubtool(selectedSubtool);
-                    break;
+                AnsiConsole.MarkupLine("[red]Database connection failed![/]");
             }
+            else
+            {
+                AnsiConsole.MarkupLine("[green]Database connection successful![/]");
+            }
+        }
+        catch (Exception ex)
+        {
+            AnsiConsole.WriteException(ex);
+        }
+
+        AppInfo appInfo = new AppInfo();
+        appInfo.DBConnect = true;
+        appInfo.On = "Online";
+
+        // Load tools from the database
+        var toolsCollection = _dbService.GetCollection<Tool>("Tool");
+        var tools = toolsCollection.Find(tool => true).ToList();
+
+        // Display tool menu
+        var toolMenu = new SelectionPrompt<string>()
+           .Title("Wähle [green]Tool[/] für Details : ")
+           .AddChoices(tools.Select(t => t.Name).ToArray());
+
+        var selectedToolName = AnsiConsole.Prompt(toolMenu);
+        var selectedTool = tools.First(t => t.Name == selectedToolName);
+
+        // Display subtool menu
+        var subtoolMenu = new SelectionPrompt<string>()
+            .Title($"Wähle [green]Subtool[/] aus [blue]{selectedTool.Name}[/] für Details / Start : ")
+            .AddChoices(selectedTool.Subtools.Select(st => st.Name).ToArray());
+
+        var selectedSubtoolName = AnsiConsole.Prompt(subtoolMenu);
+        var selectedSubtool = selectedTool.Subtools.First(st => st.Name == selectedSubtoolName);
+
+        // Display subtool details or start subtool
+        var subtoolActionMenu = new SelectionPrompt<string>()
+            .Title($"Program : [blue]{selectedSubtool.Name}[/]?")
+            .AddChoices(new[] { "Details", "Start" });
+
+        var subtoolAction = AnsiConsole.Prompt(subtoolActionMenu);
+
+        switch (subtoolAction)
+        {
+            case "Details":
+                // Display subtool details
+                var table = new Table();
+                table.AddColumn("Property");
+                table.AddColumn("Value");
+
+                table.AddRow("Name", selectedSubtool.Name);
+                table.AddRow("Update", selectedSubtool.Update);
+                table.AddRow("Author", selectedSubtool.Author);
+                table.AddRow("GitHub", selectedSubtool.GitHub);
+
+                AnsiConsole.Write(table);
+                Console.ReadLine();
+                Console.Clear();
+                break;
+            case "Start":
+                // Start the subtool
+                Console.WriteLine($"Starting [blue]{selectedSubtool.Name}[/]...");
+                // Execute the subtool
+                ExecuteSubtool(selectedSubtool);
+                break;
         }
     }
 
@@ -132,29 +134,23 @@ public class DB
     {
         try
         {
-            ProcessStartInfo startInfo = new ProcessStartInfo
+            var process = new Process
             {
-                FileName = subtool.ExecutablePath,
-                Arguments = subtool.Arguments,
-                WorkingDirectory = subtool.WorkingDirectory,
-                CreateNoWindow = true,
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true
+                StartInfo = new ProcessStartInfo
+                {
+                   // FileName = subtool.Path = "",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
             };
 
-            using Process process = Process.Start(startInfo);
-            process.OutputDataReceived += (sender, data) => Console.WriteLine(data.Data);
-            process.ErrorDataReceived += (sender, data) => Console.WriteLine($"Error: {data.Data}");
-
-            process.BeginOutputReadLine();
-            process.BeginErrorReadLine();
-
+            process.Start();
             process.WaitForExit();
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error executing subtool: {ex.Message}");
+            AnsiConsole.WriteException(ex);
         }
     }
 }
